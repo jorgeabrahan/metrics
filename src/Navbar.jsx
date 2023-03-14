81677a5b24c2391cd8f8377c8bac58a46ce5614e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-13 18:45:54 -0600

    Create navbar component layout

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,35 @@
-import { Outlet } from 'react-router-dom';
+import {
+  NavLink, Outlet, useLocation, useParams,
+} from 'react-router-dom';
+import './Navbar.css';
+
+const capitalize = (word) => (
+  `${word[0].toUpperCase()}${word.slice(1, word.length)}`
+);
 
 function Navbar() {
+  const { pathname } = useLocation();
+  const { country } = useParams();
+
+  const isShowingDetails = pathname.split('/')[1] === 'details';
+
   return (
     <>
-      <nav>
-        <i>Navbar component</i>
+      <nav className="nav center">
+        {isShowingDetails && (
+          <NavLink to="/home" className="reset">
+            <span className="material-symbols-outlined">
+              arrow_back_ios
+            </span>
+            <span>{capitalize(country)}</span>
+          </NavLink>
+        )}
+        <h1 className="nav__title">Air Pollution</h1>
+        <button className="reset" type="button">
+          <span className="material-symbols-outlined">
+            filter_list
+          </span>
+        </button>
       </nav>
 
       <Outlet />
