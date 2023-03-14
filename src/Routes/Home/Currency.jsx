1b0cf1096bad57c84677155a376be6ef5393071f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-14 01:23:16 -0600

    Create component to display each currency on the home screen

---
+++ src/Routes/Home/Currency.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Currency = ({
+  short, name,
+}) => (
+  <NavLink to={`/details/${short}`}>
+    <div>
+      <h2 className="currency__title">
+        { name }
+      </h2>
+      <small>{ short }</small>
+    </div>
+    <div className="currency__icon">
+      <span className="material-symbols-outlined">
+        arrow_right_alt
+      </span>
+    </div>
+  </NavLink>
+);
+
+Currency.propTypes = {
+  short: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Currency;
