54dfc8957824eb19678f8c86b333a427a3869e30
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-15 11:57:56 -0600

    Use the isolated wrap method from testResources for the currency component tests

--- src/tests/Currency.test.js
+++ src/tests/Currency.test.js
@@ -1,31 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import store from '../redux/store';
 import Currency from '../Routes/Home/Currency';
+import wrap from './testResources';
 
 describe('Tests for <Currency /> component', () => {
-  const createCurrency = (
+  const currency = (
     <Currency
       short="usd"
       name="United States Dollar"
     />
   );
 
-  const wrap = (component) => (
-    <Provider store={store}>
-      {component}
-    </Provider>
-  );
-
   it('Should render the content correctly', () => {
-    render(wrap(createCurrency()));
+    render(wrap(currency));
     expect(screen.getByText('usd')).not.toBeNull();
     expect(screen.getByText('United States Dollar')).not.toBeNull();
   });
 
   it('Should match the snapshot', () => {
-    expect(renderer.create(wrap(createCurrency())).toJSON()).toMatchSnapshot();
+    expect(renderer.create(wrap(currency)).toJSON()).toMatchSnapshot();
   });
 });
