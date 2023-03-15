915b7de7dd66d7fcdebc0884cec9893ce8860e31
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-14 20:39:36 -0600

    Create details component to display conversion rates

--- src/Routes/Details/Details.jsx
+++ src/Routes/Details/Details.jsx
@@ -1,7 +1,115 @@
-const Details = () => (
-  <div>
-    <p>Details</p>
-  </div>
-);
+/* eslint-disable import/no-extraneous-dependencies */
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import {
+  clearDetails, fetchCurrency, searchForConversion, setAmount,
+} from '../../redux/Details/detailsSlice';
+import Conversion from './Conversion';
+import './Details.css';
+import { setQuery } from '../../redux/Search/searchSlice';
+
+const pattern = [false, true];
+
+const isDarker = (index) => {
+  if (index < pattern.length) return pattern[index];
+  const [first, last] = pattern;
+  if (first === last) {
+    pattern[1] = !last;
+    return !last;
+  }
+  pattern[0] = !first;
+  return last;
+};
+
+const Details = () => {
+  const dispatch = useDispatch();
+  const { currency } = useParams();
+  const {
+    status, details, filteredConversions, error, amount,
+  } = useSelector((store) => store.details);
+  const { isSearching, query } = useSelector((store) => store.search);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // se verifica que sea un numero
+    const num = Number(amount);
+    if (Number.isNaN(num)) return;
+    // se verifica que sea positivo
+    if (num < 1) return;
+
+    dispatch(searchForConversion(query));
+  };
+
+  useEffect(() => {
+    if (status !== 'idle') return undefined;
+    dispatch(fetchCurrency(currency));
+
+    return () => {
+      dispatch(clearDetails());
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isSearching) return;
+    if (filteredConversions.length === 0 && isSearching) return;
+    if (status !== 'fulfilled') return;
+    dispatch(searchForConversion(''));
+    dispatch(setQuery(''));
+  }, [dispatch, isSearching]);
+
+  let render = <p>Loading...</p>;
+  if (status === 'rejected') {
+    render = (
+      <p>
+        [Error]:
+        {error}
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      {status === 'fulfilled' ? (
+        <>
+          <div className="header">
+            <form className="amount-form" onSubmit={onSubmit}>
+              <button type="submit" className="amount__submit">
+                <span className="material-symbols-outlined">
+                  autorenew
+                </span>
+              </button>
+              <input
+                className="amount"
+                type="text"
+                value={amount}
+                onChange={({ target }) => dispatch(setAmount(target.value))}
+              />
+              <h2>
+                {' '}
+                {details.short}
+                {' '}
+                rates
+              </h2>
+            </form>
+            <small>{details.date}</small>
+          </div>
+          <small className="subtitle-separator">List of conversions</small>
+          <div className="conversions">
+            {filteredConversions.map(({ name, value }, index) => (
+              <Conversion
+                key={uuid()}
+                name={name}
+                value={Number(value)}
+                isDarker={isDarker(index)}
+              />
+            ))}
+          </div>
+        </>
+      ) : render}
+    </div>
+  );
+};
 
 export default Details;
