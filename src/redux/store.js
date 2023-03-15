a6635a15c1c386954ca10518062648033c25297e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-14 20:41:16 -0600

    Add details slice to the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configureStore } from '@reduxjs/toolkit';
 import currenciesSlice from './Currencies/currenciesSlice';
+import detailsSlice from './Details/detailsSlice';
 import searchSlice from './Search/searchSlice';
 
 const store = configureStore({
   reducer: {
     currencies: currenciesSlice,
     search: searchSlice,
+    details: detailsSlice,
   },
 });
 
