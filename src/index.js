934e6354cee30a57fab96aab599ffef7a2b64024
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-15 13:17:36 -0600

    Implement router from external file

--- src/index.js
+++ src/index.js
@@ -2,35 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Navbar from './Navbar';
-import ErrorPage from './ErrorPage';
-import Home from './Routes/Home/Home';
-import Details from './Routes/Details/Details';
 import store from './redux/store';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navbar />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'home',
-        element: <Home />,
-      },
-      {
-        path: 'details/:currency',
-        element: <Details />,
-      },
-    ],
-  },
-]);
+import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
