80e773005e45686406018ace4006ead0b18b3319
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-15 12:24:35 -0600

    Create tests for conversion component

---
+++ src/tests/Conversion.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Conversion from '../Routes/Details/Conversion';
+import wrap from './testResources';
+
+describe('Tests for <Conversion /> component', () => {
+  const conversion = (
+    <Conversion
+      name="usd"
+      value={120}
+      isDarker={false}
+    />
+  );
+
+  it('Should render the content correctly', () => {
+    render(wrap(conversion));
+    expect(screen.getByText('usd')).not.toBeNull();
+    expect(screen.getByText('120')).not.toBeNull();
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderer.create(wrap(conversion)).toJSON()).toMatchSnapshot();
+  });
+});
